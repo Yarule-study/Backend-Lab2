64b766f208805f34e6e9e90131be51351b26eea2
Yaroslav Svidersky <megayara12@gmail.com>
2025-01-10 15:35:27 +0200

    Add Entities

---
+++ db/entities.py
@@ -0,0 +1,16 @@
+from marshmallow import Schema, fields, validate
+
+class UserSchema:
+    id = fields.Int(dump_only=True, validate=validate.Range(min=1))
+    name = fields.Str(required=True)
+
+class CategorySchema:
+    id = fields.Int(dump_only=True, validate=validate.Range(min=1))
+    name = fields.Str(required=True)
+
+class RecordSchema:
+    id = fields.Int(dump_only=True, validate=validate.Range(min=1))
+    user_id = fields.Int(required=True, validate=validate.Range(min=1))
+    category_id = fields.Int(required=True, validate=validate.Range(min=1))
+    timestamp = fields.DateTime(format="%d/%m/%Y %H:%M:%S", dump_only=True)
+    spent = fields.Float(required=True, validate=validate.Range(min=0))
